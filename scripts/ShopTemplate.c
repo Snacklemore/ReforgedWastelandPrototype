1d324cef6ce98fa11eac11b0403913c0818c166e
Snacklemore <best-of-nico@gmx.net>
2023-04-11 23:55:54 +0200

    shop template, cleanups

---
+++ scripts/ShopTemplate.c
@@ -0,0 +1,358 @@
+
+///////////////////////////////////////////////////////////////////////------------------PreviewSetup
+class WST_Equipment
+{
+	//"suppressed" variants are capable of fitting a suppressor and not actually suppressed
+	
+	/////////////////////////////////////////////////////////////////////////
+	//prefab item paths
+	
+	//weapon
+	ref array<ShopObject> shopItems = new array<ShopObject>();
+	
+	
+	ref ShopObject AN94O = new ShopObject("{AF2B2A5270708E19}Prefabs/Weapons/Rifles/AN94/Rifle_AN94_1P63.et","WeaponAN94","AN 74 with 1P63",WST_Type.WST_WEAPON,300);
+
+
+	
+	ref map<string,ResourceName> buildArray = new map<string,ResourceName>();
+	ref array<string> iterationArray = new array<string>();
+	ref map<string,string> DisplayNameArray = new map<string,string>();
+	ref map<string,int> TypeArray = new map<string,int>();
+	ref map<string,int> PriceArray = new map<string,int>();
+
+	private WST_Weapon instance;
+	WST_Weapon GetInstance()
+	{
+		if(instance)
+			return instance;
+		else
+			return new WST_Weapon();
+	}
+	
+	int GetPriceByKey(string identifier)
+	{
+		return PriceArray.Get(identifier);
+	}
+	void WST_Weapon()
+	{
+		if (instance)
+			return;
+		instance = this;
+		//
+		shopItems.Insert(AK74O);
+		
+		
+		foreach(ShopObject o:shopItems)
+		{
+			buildArray.Insert(o.m_Identifier,o.m_prefabPath);
+			iterationArray.Insert(o.m_Identifier);
+			DisplayNameArray.Insert(o.m_Identifier,o.m_Display);
+			TypeArray.Insert(o.m_Identifier,o.m_Type);
+			PriceArray.Insert(o.m_Identifier,o.m_price);
+		
+		}
+		
+		
+		
+	}
+	
+}
+
+
+//TODO:add proper hints
+class WST_ShopTemplate : MenuBase
+{
+	protected static const string TEXT_TITLE = "TextTitle";
+	protected static const string BUTTON_CLOSE = "ButtonClose";
+	protected static const string BUTTON_BUY = "ButtonBuy";
+	SCR_TabViewComponent m_TabView;
+	BlurWidget wBlur ;
+	bool firstSelection = true;
+	ref WST_Weapon w;
+
+	ResourceName spawnEntity;
+	WST_TraderComponent TrdComp;
+	int balance;
+	
+	protected void Buy()
+	{
+		InventorySearchPredicate search;
+
+
+		PlayerController pc = GetGame().GetPlayerController();
+		IEntity ie = pc.GetControlledEntity();
+		InventoryStorageManagerComponent storage = InventoryStorageManagerComponent.Cast(ie.FindComponent(InventoryStorageManagerComponent));
+
+		array<typename> components = {};
+		components.Insert(MoneyComponent);
+		components.Insert(RplComponent);
+		IEntity walletEnity = storage.FindItemWithComponents(components, EStoragePurpose.PURPOSE_DEPOSIT);
+
+		MoneyComponent wallet;
+		if(walletEnity)
+		{
+			wallet = MoneyComponent.Cast(walletEnity.FindComponent(MoneyComponent));
+		}
+		WST_TraderComponent traderComp = WST_TraderComponent.Cast(ie.FindComponent(WST_TraderComponent));
+		if (!traderComp)
+		{
+			Print("WST_TransferWindow::Buy::No TraderComponent!! ");
+			return;
+		
+		}
+		
+		traderComp.HandleBuyAction(spawnEntity,balance);
+		
+		
+		
+
+	}
+	///////////////////////////////////////////////////////////////////////------------------PreviewSetup
+	
+	private ref array<ref ManagedDataObject> dataObjects = new array<ref ManagedDataObject>();
+	void setupDataObjects()
+	{
+		
+		
+		//iterate through iterationArray for keys for data map
+		
+		for (int i = 0;i < w.iterationArray.Count();i++)
+		{
+		
+			string key = w.iterationArray.Get(i);
+			ManagedDataObject o = new ManagedDataObject();
+			o.SetData(key);
+			o.SetType(w.TypeArray.Get(key));
+			o.SetDisplayText(w.DisplayNameArray.Get(key));
+			dataObjects.Insert(o);
+		}
+	}
+
+	void OnSelected(SCR_ListBoxComponent list , int itemIndex, bool isTheNewSelection)
+	{
+		///////////////////////////////////////////////////////////////////////------------------PreviewSetup
+
+		Print("WST_TransferWindow::OnSelectedInvoker");
+		Widget rootWidget = GetRootWidget();
+
+		ManagedDataObject data =  list.GetItemData(itemIndex);
+		Widget panelRoot = rootWidget.FindWidget("Panel0");
+		ItemPreviewWidget preview = panelRoot.FindWidget("WeaponPreview");
+		string weapon
+		if (data)
+			weapon = data.GetData();
+		
+		foreach (ManagedDataObject dataObject : dataObjects)
+		{
+			if (dataObject.GetData() == weapon)
+			{
+				
+				
+				//"weapon" is the key for the hashmap
+				
+				ResourceName WeaponEntityPrefab = w.buildArray.Get(weapon);
+				//ResourceName needed here!!
+				GetGame().GetItemPreviewManager().SetPreviewItemFromPrefab(preview,WeaponEntityPrefab,null,false);
+
+			}
+		}
+
+
+
+		///////////////////////////////////////////////////////////////////////------------------Trading CP_Logic
+		InventorySearchPredicate search;
+
+
+		PlayerController pc = GetGame().GetPlayerController();
+		IEntity ie = pc.GetControlledEntity();
+		InventoryStorageManagerComponent storage = InventoryStorageManagerComponent.Cast(ie.FindComponent(InventoryStorageManagerComponent));
+
+		array<typename> components = {};
+		components.Insert(MoneyComponent);
+		components.Insert(RplComponent);
+		IEntity walletEnity = storage.FindItemWithComponents(components, EStoragePurpose.PURPOSE_DEPOSIT);
+
+		MoneyComponent wallet;
+		if(walletEnity)
+		{
+			wallet = MoneyComponent.Cast(walletEnity.FindComponent(MoneyComponent));
+		}
+		WST_TraderComponent traderComp = WST_TraderComponent.Cast(ie.FindComponent(WST_TraderComponent));
+		if (traderComp) 
+		{
+	    TrdComp = traderComp;
+	
+			
+			
+			
+			foreach (string key : w.iterationArray)
+			{
+				if (key == weapon)
+				{
+					if (!wallet) 
+					{
+						spawnEntity ="";
+
+		            	Print("WST_TransferWindowUI::OnSelected::noWallet");
+		            	return;
+						
+						
+	        		}
+					balance = wallet.GetValue();
+				    balance = balance - w.GetPriceByKey(key);//w.GetWeaponPriceByKey(key);
+				
+				    if (balance < 0) 
+					{
+						spawnEntity = "";
+			
+				         Print("WST_TransferWindowUI::OnSelected::noBalance");
+				         return;
+				     }
+					ResourceName n = w.buildArray.Get(key);
+				    spawnEntity = n;
+				}
+			}
+			
+		
+		
+		}
+
+	}
+
+
+
+	//------------------------------------------------------------------------------------------------
+	protected override void OnMenuOpen()
+	{
+		Print("OnMenuOpen: menu/dialog opened!", LogLevel.NORMAL);
+		w = new WST_Equipment();
+		setupDataObjects();
+		Widget rootWidget = GetRootWidget();
+		if (!rootWidget)
+		{
+			Print("Error in Layout Tutorial layout creation", LogLevel.ERROR);
+			return;
+		}
+
+
+
+
+		Widget panelRoot = rootWidget.FindWidget("Panel0");
+		ItemPreviewWidget preview = panelRoot.FindWidget("WeaponPreview");
+
+		
+		
+
+		m_ListBoxOverlay =  OverlayWidget.Cast(rootWidget.FindAnyWidget("ListBoxWeapons"));
+		m_ListBoxComponent = SCR_ListBoxComponent.Cast(m_ListBoxOverlay.FindHandler(SCR_ListBoxComponent));
+        if (m_ListBoxComponent)
+        {
+			
+			foreach (ManagedDataObject o : dataObjects)
+			{
+				if (o.GetType() == WST_Type.WST_WEAPON)
+				{
+					string price = o.GetData();
+					int i_price = w.GetPriceByKey(price);
+					price = i_price.ToString();
+					m_ListBoxComponent.AddItem(o.GetDisplayText() +"  $$: "+price ,o);
+				}
+				
+			}
+           
+
+
+
+        }
+
+
+		m_ListBoxOverlay1 =  OverlayWidget.Cast(rootWidget.FindAnyWidget("ListBoxAttachments"));
+		 m_ListBoxComponent1 = SCR_ListBoxComponent.Cast(m_ListBoxOverlay1.FindHandler(SCR_ListBoxComponent));
+        if (m_ListBoxComponent1)
+        {
+			foreach (ManagedDataObject o : dataObjects)
+			{
+				if (o.GetType() == WST_Type.WST_ATTACHMENT)
+				{
+					string price = o.GetData();
+					int i_price = w.GetPriceByKey(price);
+					price = i_price.ToString();
+					m_ListBoxComponent1.AddItem(o.GetDisplayText() +"  $$: "+price ,o);
+				}
+				
+			}
+           
+
+        }
+
+
+
+
+		//OnSelectInvoker
+		m_ListBoxComponent.m_OnChanged.Insert(OnSelected);
+		m_ListBoxComponent1.m_OnChanged.Insert(OnSelected);
+		m_ListBoxComponent2.m_OnChanged.Insert(OnSelected);
+		m_ListBoxComponent3.m_OnChanged.Insert(OnSelected);
+
+
+		SCR_ButtonTextComponent buttonClose = SCR_ButtonTextComponent.GetButtonText(BUTTON_CLOSE, rootWidget);
+		if (buttonClose)
+			buttonClose.m_OnClicked.Insert(Close);
+		else
+			Print("Button Close not found - won't be able to exit by button", LogLevel.WARNING);
+
+		/*
+			buy  button
+		*/
+
+		SCR_ButtonTextComponent buttonChange = SCR_ButtonTextComponent.GetButtonText(BUTTON_BUY, rootWidget);
+		if (buttonChange)
+			buttonChange.m_OnClicked.Insert(Buy);
+		else
+			Print("Button Change not found", LogLevel.WARNING); // the button can be missing without putting the layout in jeopardy
+
+		/*
+			ESC/Start listener
+		*/
+
+		InputManager inputManager = GetGame().GetInputManager();
+		if (inputManager)
+		{
+			// this is for the menu/dialog to close when pressing ESC
+			// an alternative is to have a button with the SCR_NavigationButtonComponent component
+			// and its Action Name field set to MenuBack - this would activate the button on ESC press
+			inputManager.AddActionListener("MenuOpen", EActionTrigger.DOWN, Close);
+			inputManager.AddActionListener("MenuBack", EActionTrigger.DOWN, Close);
+#ifdef WORKBENCH // in Workbench, F10 is used because ESC closes the preview
+			inputManager.AddActionListener("MenuOpenWB", EActionTrigger.DOWN, Close);
+			inputManager.AddActionListener("MenuBackWB", EActionTrigger.DOWN, Close);
+#endif
+		}
+		else if (!buttonClose)
+		{
+			Print("Auto-closing the menu that has no exit path", LogLevel.WARNING);
+			Close();
+			return;
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected override void OnMenuClose()
+	{
+		// here we clean action listeners added above as the good practice wants it
+		InputManager inputManager = GetGame().GetInputManager();
+		if (inputManager)
+		{
+			inputManager.RemoveActionListener("MenuOpen", EActionTrigger.DOWN, Close);
+			inputManager.RemoveActionListener("MenuBack", EActionTrigger.DOWN, Close);
+#ifdef WORKBENCH
+			inputManager.RemoveActionListener("MenuOpenWB", EActionTrigger.DOWN, Close);
+			inputManager.RemoveActionListener("MenuBackWB", EActionTrigger.DOWN, Close);
+#endif
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+
+};
